eabb2f62e87efa68096f5624a44c0472d111b586
Sebastian Barreto <sebastian.barreto2002@gmail.com>
2024-07-15 13:55:19 -0500

    actualizacion de la clase cliente

--- src/main/java/com/mycompany/proyectoaa2ev02/entidadCliente.java
+++ src/main/java/com/mycompany/proyectoaa2ev02/entidadCliente.java
@@ -10,4 +10,70 @@ package com.mycompany.proyectoaa2ev02;
  */
 public class entidadCliente {
     
+    
+    int cedula;
+    String nombre;
+    String apellido;
+    int edad;
+    String correo;
+    String clave;
+    String telefono;
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+            
+    
 }
